86cfd7259af25954dc79909f5062c6792e4854ef
odonogc <43205673+odonogc@users.noreply.github.com>
2018-09-12 12:29:15 +0100

    Latest version

---
+++ StarshipCalculator/StarshipCalculator/StarshipModel.cs
@@ -0,0 +1,43 @@
+﻿
+
+namespace StarshipCalculator
+{
+    /// <summary>
+    /// This class represents a model of starship in the system
+    /// </summary>
+public    class Starship
+
+    {
+        public Starship()
+        {
+
+        }
+
+        public Starship(string shipName, decimal fuelRange)
+        {
+            ShipName = shipName;
+            FuelRange = fuelRange;
+
+        }
+
+
+        /// <summary>
+        /// The name of the model of Starship
+        /// </summary>
+        public string ShipName { get; set; }
+       
+
+        /// <summary>
+        /// The range in MGLT for a full load of fuel
+        /// </summary>
+        public decimal FuelRange { get; set; }
+
+
+        /// <summary>
+        /// The numberof fuel stops requried to complete a journey
+        /// </summary>
+        public int FuelStops { get; set; }
+        
+    }
+
+}
---
+++ StarshipCalculator/StarshipCalculator/UnitTest1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StarshipCalculator;
+
+namespace StarshipUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CreateStarshipConstructor1()
+        {
+
+            var ship = new Starship();
+
+            Assert.IsNotNull(ship);
+            
+        }
+
+        [TestMethod]
+        public void CreateStarshipConstructor2()
+        {
+
+            var ship = new Starship("TestName", 12.5M);
+
+            Assert.IsNotNull(ship);
+
+        }
+
+        [TestMethod]
+        public void GetStarshipsTest()
+        {
+
+            var result = Program.GetStarships();
+
+            Assert.IsNotNull(result);
+            
+        }
+
+        [TestMethod]
+        public void GetFuelRangeTest1()
+        {
+
+            var result = Program.GetFuelStops(10,100);
+
+            Assert.AreEqual(result, 9);
+
+
+        }
+
+        [TestMethod]
+        public void GetFuelRangeTest2()
+        {
+
+            var result = Program.GetFuelStops(15, 100);
+
+            Assert.AreEqual(result, 6);
+
+
+        }
+
+        [TestMethod]
+        public void GetFuelRangeTest3()
+        {
+
+            var result = Program.GetFuelStops(0, 100);
+
+            Assert.AreEqual(result, 0);
+            
+        }
+
+        [TestMethod]
+        public void GetFuelRangeTest4()
+        {
+
+            var result = Program.GetFuelStops(100, 0);
+
+            Assert.AreEqual(result, 0);
+
+        }
+
+        public void GetFuelRangeTest5()
+        {
+            //Range is larger than distance
+
+            var result = Program.GetFuelStops(10, 100);
+
+            Assert.AreEqual(result, 0);
+
+        }
+    }
+}
